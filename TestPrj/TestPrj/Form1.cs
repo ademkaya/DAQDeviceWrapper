e791e1d89ef3fd2042bb5369a7c5e89cc4f8c423
Adem <ademkaya@gmail.com>
2021-09-14 10:29:05 +0300

    tested, works fine.

---
+++ TestPrj/TestPrj/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        DAQ.Device NI6361;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (NI6361 == null)
+                NI6361 = new DAQ.Device();      //build it with x86 conf.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        int dataCount = 0;
+        void readDataCallBack(IAsyncResult result)
+        {
+            if (NI6361 == null)
+                return;
+
+            string s = string.Empty;
+            double[] DAQArray = NI6361.GetData(result);
+
+            Invoke(new Action(() => { Text = "Data Received " + (dataCount++).ToString(); }));
+            for (int h = 0; h < DAQArray.Length; h++)
+            {
+                s = DAQArray[h].ToString() + ";" + s;
+            }
+            Invoke(new Action(() => { textBox1.Text = s; }));
+
+            DAQArray = null;
+        }
+
+        bool firstStart = true;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (firstStart)
+            {
+                string phyChnl = "Dev1/ai0";
+                string extClockChnl = "/Dev1/PFI0";  /*  or string.Empty to asynchronous sampling    */;
+                double minVoltage = -4.0F;
+                double maxVoltage = 4.0F;
+                double samplingRate = 100;
+                int sampleCount = 10;               // (int)(0.25 /*second*/ * samplingRate);
+
+                NI6361.Connect(phyChnl, extClockChnl, minVoltage, maxVoltage, DAQ.SamplingMode.Continuous, samplingRate, sampleCount, readDataCallBack);
+                NI6361.Start();
+
+                firstStart = false;
+                button1.Text = "stop";
+
+            }
+            else
+            {
+                if (NI6361.IsStopped)
+                {
+                    NI6361.Restart();
+                    button1.Text = "stop";
+                }
+                else
+                {
+                    NI6361.Stop();
+                    button1.Text = "start";
+                }
+            }
+        }
+    }
+}
---
+++ daqDeviceWrapper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using NationalInstruments;
+using NationalInstruments.DAQmx;
+
+namespace DAQ
+{
+    public class Device:IDisposable
+    {
+
+        private AnalogWaveform<double>[] data;
+        private returnPtr returStr;
+
+        private double  _samplingRate = 0;
+        private int     _samplesPerChannel = 0;
+        private bool    _stop = true;
+        private bool    _DAQErrorOccured = false;
+
+        public bool     IsStopped { get { return _stop; } }
+        public double   SamplingRate { get { return _samplingRate; } }
+        public int      samplesPerChannel { get { return _samplesPerChannel; } }
+        public bool     DAQErrorOccured { get { return _DAQErrorOccured; } }
+
+        public Device()
+        {
+            // Create a new task
+            returStr.Task = new NationalInstruments.DAQmx.Task();
+        }
+
+        /* this class is modified for a single channel only, must be updated in order to work in multiple channels simultaneously*/
+        public delegate void Callback(IAsyncResult result);
+        public returnPtr Connect(string AnalogInChannel,string ExtClockChannel,double minVoltage,double maxVoltage, SamplingMode mode,double samplingRate,int sampleCount, Callback callbackFunc)
+        {
+            _samplingRate = samplingRate;
+            _samplesPerChannel = sampleCount;
+
+                returStr.Task.AIChannels.CreateVoltageChannel(AnalogInChannel, "", (AITerminalConfiguration)(-1), minVoltage, maxVoltage, AIVoltageUnits.Volts);
+
+                // Configure timing specs    
+                SampleQuantityMode _mode = (SampleQuantityMode)mode;
+                returStr.Task.Timing.ConfigureSampleClock(ExtClockChannel, samplingRate, SampleClockActiveEdge.Rising, _mode, sampleCount);
+
+            // Verify the task
+            returStr.Task.Control(TaskAction.Verify);
+
+            returStr.channelReader = new AnalogMultiChannelReader(returStr.Task.Stream);
+            returStr.callBackPtr = new AsyncCallback(callbackFunc);
+
+            returStr.channelReader.SynchronizeCallbacks = true;
+
+            return returStr;
+        }
+
+        public void Dispose()
+        {
+            returStr.Task.Dispose();
+        }
+
+        public void Start()
+        {
+            _stop = false;
+            this._DAQErrorOccured = false;
+
+            if (this.returStr.Task == null)
+                return;
+
+            this.returStr.channelReader.BeginReadWaveform(this._samplesPerChannel, this.returStr.callBackPtr, this.returStr.Task);
+        }
+
+        public void Restart()
+        {
+            _stop = false;
+            this._DAQErrorOccured = false;
+
+
+            if (this.returStr.Task == null)
+                return;
+
+            this.returStr.channelReader.BeginMemoryOptimizedReadWaveform(this._samplesPerChannel, this.returStr.callBackPtr, this.returStr.Task, this.data);
+        }
+
+        public void Stop()
+        {
+            _stop = true;
+            this._DAQErrorOccured = false;
+        }
+
+        /* called in the callback function */
+        public double[] GetData(IAsyncResult ar)
+        {
+            try
+            {
+                this.data = this.returStr.channelReader.EndReadWaveform(ar);
+
+                if (!this._stop)
+                    Restart();
+            }
+            catch (Exception e)
+            {
+                this._DAQErrorOccured = true;
+                Console.WriteLine(e.Message);
+            }
+
+            return data[0].GetRawData(0, data[0].SampleCount);
+        }
+
+    }
+
+    public static class Channels
+    {
+        public static string[] ReturnChannels()
+        {
+            return DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AI, PhysicalChannelAccess.External);
+        }
+    }
+
+    public struct returnPtr
+    {
+        public AnalogMultiChannelReader channelReader;
+        public AsyncCallback callBackPtr;
+        public NationalInstruments.DAQmx.Task Task;
+    }
+
+    public enum SamplingMode
+    {
+        Continuous = SampleQuantityMode.ContinuousSamples,
+        Finite = SampleQuantityMode.FiniteSamples,
+        HardwareTimedSinglePoint = SampleQuantityMode.HardwareTimedSinglePoint
+    }
+
+}
